41f840f84aadce995a07014c24d0bd868719c55a
tonovarela <tonovarela@live.com>
2025-12-03 17:19:21 -0600

    Agrega soporte para carga y procesamiento de archivos ZIP en el componente REPSE; incluye la biblioteca JSZip y métodos para leer archivos XML.

--- src/app/pages/repse/repse.component.ts
+++ src/app/pages/repse/repse.component.ts
@@ -1,8 +1,26 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription,from,of  } from 'rxjs';
 import { OpinionCumplimiento, Usuario } from 'src/app/models';
 import { ProveedorService,SubirArchivoService,UsuarioService,ModalUploadService } from 'src/app/services';
+
+
+import * as JSZip from 'jszip/dist/jszip';
+import { switchMap, tap, filter, first } from 'rxjs/operators';
+
+ interface ZipFile {
+   name: string;
+   dir: boolean;
+   date: Date;
+   data: any;
+}
+
+interface ResponseXML {
+  emisor: string;
+  receptor: string;
+  fecha: Date;
+  error?: {mensaje:string,fileName:string};
+}
 
 
 @Component({
@@ -64,6 +82,73 @@ ngOnDestroy(): void {
       .subscribe(data => {
         this.historicoOpinionCumplimiento = data.documentos;
       });
+  }
+
+
+  async leerArchivoXML(fileZip:any):Promise<ResponseXML>{     
+        const parser = new DOMParser();
+        const contenidoDelArchivo = await fileZip.async('string');
+        const xmlDoc = parser.parseFromString(contenidoDelArchivo,"text/xml");
+                              
+        try {
+
+          const root = xmlDoc.childNodes[0];   
+          const fecha = (root as Element).attributes["Fecha"].value;          
+          const nodes= root.childNodes;        
+          const nodesArray = Array.from(nodes);
+          const [emisor] =nodesArray.filter(n=>n.nodeName=="cfdi:Emisor");
+          const [receptor] =nodesArray.filter(n=>n.nodeName=="cfdi:Receptor");        
+          const emisorNode = emisor as Element;
+          const receptorNode = receptor as Element;
+          console.log({
+                  emisor:emisorNode.attributes["Rfc"].value,
+                  receptor:receptorNode.attributes["Rfc"].value,
+                  fecha,
+                  error:null
+                 });
+           return {
+                  emisor:emisorNode.attributes["Rfc"].value,
+                  receptor:receptorNode.attributes["Rfc"].value,
+                  fecha:new Date(fecha),                  
+                  error:null
+                 };           
+          
+        } catch (error) {
+          return {
+            emisor:null,
+            receptor:null,
+            fecha:null,
+            error: {mensaje:error.message,fileName:fileZip.name},          
+          };
+        }
+                
+  }
+
+
+
+  async ngOnFile(event: any) {    
+    const fileList = event.target.files;
+    const zipLoaded = new JSZip.default();      
+    const promise =from(zipLoaded.loadAsync(fileList[0])).pipe(
+      switchMap((zip: any):Observable<ZipFile[]> => {
+        return of(Object.keys(zip.files).map((key)=>zip.files[key]))        
+      })).toPromise();
+
+    const filesPromise = await promise;      
+    const files = filesPromise                          
+                          .filter(f=>!(f.name.includes("__MACOSX")))                            
+                          .filter(f=>!(f.name.includes(".DS_Store")))
+                          .filter(f=>!f.dir)
+                          .filter(f=>f.name.toLowerCase().endsWith(".xml"));                            
+                            
+    for (const file of files) {
+        const responseXML= await this.leerArchivoXML(file);        
+        console.log(responseXML);
+        
+    }
+                                        
+
+    
   }
 
 }
